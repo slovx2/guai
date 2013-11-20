29180f876d03b85d8460aa9f9cdba10eda9a2d30
slovx2 <slovx2@gmail.com>
2013-11-20 18:33:51 +0800

    根据技能捡出装备

--- Tmp/routes/post.js
+++ Tmp/routes/post.js
@@ -2,13 +2,29 @@
  * Created with JetBrains WebStorm.
  * User: Simon
  * Date: 13-11-19
- * Time: 下午3:40
- * To change this template use File | Settings | File Templates.
- */
-var util = require('../util/util')
-
+    * Time: 下午3:40
+* To change this template use File | Settings | File Templates.
+    */
+//var util = require('../util/util');
+var Skill = require('../model/Skill');
 exports.search = function(req, res) {
-    var skills = JSON.parse(req.body.skills);
-    console.log(util.size(skills));
-    res.send('---------------');
+    var reqData = JSON.parse(req.body.skills);
+    var reqSkills = {};
+    for (var i in reqData) {
+        reqSkills[i] = Skill.skills[i];
+    }
+
+    var fitBody = {};
+    for (var i in reqSkills) {
+        var skill = reqSkills[i];
+        var linkBodys = skill.bodys;
+        for (var j in linkBodys) {
+            fitBody[j] = linkBodys[j];
+        }
+    }
+    for (var i in fitBody) {
+        console.log(i);
+        console.log(fitBody[i].skillSyss);
+    }
+    res.send('----------');
 }
